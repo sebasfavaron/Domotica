9c2229d6c7c730cfd993f0a17bb418931699b5c2
Sebas <sebastianfavaron@gmail.com>
2018-06-02 12:48:46 -0300

    Meto Devices

---
+++ app/src/main/java/com/itba/hci/domotica/Device.java
@@ -0,0 +1,63 @@
+package com.itba.hci.domotica;
+
+public class Device {
+    private String name;
+    private String id;
+    private String typeId;
+    private String meta;
+
+    public Device(String name, String type){
+        this.name = name;
+        this.typeId = Device.typeToId(type);
+        this.meta = "";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static String typeToId(String type){
+        switch (type) {
+            case "lamp":
+                return "go46xmbqeomjrsjr";
+            case "blind":
+                return "eu0v2xgprrhhg41g";
+            case "oven":
+                return "im77xxyulpegfmv8";
+            case "ac":
+                return "li6cbv5sdlatti0j";
+            case "door":
+                return "lsf78ly0eqrjbz91";
+            case "alarm":
+                return "mxztsyjzsrq7iaqc";
+            case "timer":
+                return "ofglvd9gqX8yfl3l";
+            case "refrigerator":
+                return  "rnizejqr2di0okho";
+            default:
+                throw new RuntimeException("Wrong type");
+        }
+    }
+    public static String idToType(String id){
+        switch (id) {
+            case "go46xmbqeomjrsjr":
+                return "lamp";
+            case "eu0v2xgprrhhg41g":
+                return "blind";
+            case "im77xxyulpegfmv8":
+                return "oven";
+            case "li6cbv5sdlatti0j":
+                return "ac";
+            case "lsf78ly0eqrjbz91":
+                return "door";
+            case "mxztsyjzsrq7iaqc":
+                return "alarm";
+            case "ofglvd9gqX8yfl3l":
+                return "timer";
+            case "rnizejqr2di0okho":
+                return "refrigerator";
+            default:
+                throw new RuntimeException("Wrong id");
+        }
+    }
+}

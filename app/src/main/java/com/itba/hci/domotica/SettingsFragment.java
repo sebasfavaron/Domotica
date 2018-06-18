a3cd487f3eadf880eab551cebb2bdf32b808710a
Sebas <sebas.favaron@hotmail.com>
2018-06-18 17:38:41 -0300

    a

--- app/src/main/java/com/itba/hci/domotica/SettingsFragment.java
+++ app/src/main/java/com/itba/hci/domotica/SettingsFragment.java
@@ -20,11 +20,11 @@ public class SettingsFragment extends DialogFragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.settings, container);
-        Switch notificationSwitch = (Switch) view.findViewById(R.id.EnableDeviceNotificationSwitch);
-        Switch routineSwitch = (Switch) view.findViewById(R.id.EnableRoutineNotificationDevice);
-        notificationSwitch.setChecked(true);
+        Switch deviceSwitch = (Switch) view.findViewById(R.id.enable_device_notification_switch);
+        Switch routineSwitch = (Switch) view.findViewById(R.id.enable_routine_notification_switch);
+        deviceSwitch.setChecked(true);
         routineSwitch.setChecked(true);
-        notificationSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        deviceSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
@@ -49,9 +49,9 @@ public class SettingsFragment extends DialogFragment {
                 }
             }
         });
-        Spinner setUpdatingTime = (Spinner) view.findViewById(R.id.set_updating_time_spinner);
 
-        AdapterView.OnItemSelectedListener itemSelectedListener = new AdapterView.OnItemSelectedListener() {
+        Spinner setUpdatingTime = (Spinner) view.findViewById(R.id.set_updating_time_spinner);
+        setUpdatingTime.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 time = parent.getItemAtPosition(position).toString();
@@ -61,16 +61,14 @@ public class SettingsFragment extends DialogFragment {
             public void onNothingSelected(AdapterView<?> parent) {
 
             }
-        };
-        setUpdatingTime.setOnItemSelectedListener(itemSelectedListener);
+        });
         List<String> updatingTimes = new ArrayList<>();
         updatingTimes.add(getString(R.string.updating_time1));
         updatingTimes.add(getString(R.string.updating_time2));
         updatingTimes.add(getString(R.string.updating_time3));
         updatingTimes.add(getString(R.string.updating_time4));
         updatingTimes.add(getString(R.string.updating_time5));
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), R.layout.support_simple_spinner_dropdown_item, updatingTimes);
-        setUpdatingTime.setAdapter(adapter);
+        setUpdatingTime.setAdapter(new ArrayAdapter<>(getContext(), R.layout.support_simple_spinner_dropdown_item, updatingTimes));
         return view;
     }
 }

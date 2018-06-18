13788af51d094873b056363bacbb19026cd3f2c9
Lo Coco <jlococo@itba.edu.ar>
2018-06-18 00:13:26 -0300

    Agrego deviceAction

--- app/src/main/java/com/itba/hci/domotica/GsonRequest.java
+++ app/src/main/java/com/itba/hci/domotica/GsonRequest.java
@@ -11,12 +11,15 @@ import com.android.volley.Response;
 import com.android.volley.toolbox.HttpHeaderParser;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonSyntaxException;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.Map;
 
 public class GsonRequest<T1, T2> extends Request<T2> {
@@ -58,9 +61,8 @@ public class GsonRequest<T1, T2> extends Request<T2> {
             String json = new String(
                     response.data,
                     HttpHeaderParser.parseCharset(response.headers));
-            Log.d("tag",json);
-
             if (token != null) {
+
                 JSONObject jsonObject = new JSONObject(json);
 
                 if (clazz == Boolean.class) {
@@ -72,13 +74,10 @@ public class GsonRequest<T1, T2> extends Request<T2> {
                 } else if (clazz == Long.class) {
                     json = (new Long(jsonObject.getLong(token))).toString();
                 } else {
-                    Log.d("tag", "esto se imprime");
                     json = jsonObject.getJSONObject(token).toString();
-                    Log.d("tag",json);
-                    Log.d("tag", "esto  no se imprime");
                 }
             }
-
+            Log.d("tag", json);
             return Response.success(
                     gson.fromJson(json, clazz),
                     HttpHeaderParser.parseCacheHeaders(response));

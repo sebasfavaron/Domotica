5a6785406e341cd33636a50754b49c373759ae6c
Lo Coco <jlococo@itba.edu.ar>
2018-06-19 04:04:12 -0300

    execute notification

--- app/src/main/java/com/itba/hci/domotica/NotificationReceiver.java
+++ app/src/main/java/com/itba/hci/domotica/NotificationReceiver.java
@@ -3,6 +3,7 @@ package com.itba.hci.domotica;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -12,11 +13,15 @@ import com.android.volley.VolleyError;
 import java.util.ArrayList;
 
 public class NotificationReceiver extends BroadcastReceiver{
-
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d("tag", "Corrieron notificaciones");
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context,String.valueOf((int)(Math.random()*10000)))
+                .setContentTitle("")
+                .setContentText("");
         //todo: ejecutar busqueda de eventos, parsearlos y notificarlos
+
+        /*
         String requestTag = Api.getInstance(context).getDeviceEvents(new Response.Listener<GetDeviceEventsResponse>() {
             @Override
             public void onResponse(GetDeviceEventsResponse response) {
@@ -30,7 +35,8 @@ public class NotificationReceiver extends BroadcastReceiver{
                 Log.e("tag", error.toString());
             }
         });
-    }
+        */
 
+    }
 }
 
